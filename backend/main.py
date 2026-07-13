948b75340ba255ee183170addfddafad84f0ce96
QEVoyo <978745130@qq.com>
2026-07-13 21:20:33 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,6 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:8000",
-        "https://frontend-ebon-gamma-45.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
